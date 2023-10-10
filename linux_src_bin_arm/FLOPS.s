eaff1debb31fe041b82c4fd488e929fb11d5f05b
Maneren <49210777+Maneren@users.noreply.github.com>
2023-10-10 10:40:02 +0200

    remove avx512 for arm

--- linux_src_bin_arm/FLOPS.s
+++ linux_src_bin_arm/FLOPS.s
@@ -33,24 +33,6 @@
 	fadd	s15, s15, s0
 .endm
 
-.macro fadd_AVX512
-	fadd	s1, s1, s0
-	fadd	s2, s2, s0
-	fadd	s3, s3, s0
-	fadd	s4, s4, s0
-	fadd	s5, s5, s0
-	fadd	s6, s6, s0
-	fadd	s7, s7, s0
-	fadd	s8, s8, s0
-	fadd	s9, s9, s0
-	fadd	s10, s10, s0
-	fadd	s11, s11, s0
-	fadd	s12, s12, s0
-	fadd	s13, s13, s0
-	fadd	s14, s14, s0
-	fadd	s15, s15, s0
-.endm;
-
 FLOPS_SSE:
 	mov x0, 2097152
 
@@ -96,22 +78,3 @@ FLOPS_AVX:
 
 	bne .AVXLoop
 	ret
-
-
-
-FLOPS_AVX512:
-	ret
-	mov x0, 524288
-
-.AVX512Head:
-	fadd_AVX512
-	fadd_AVX512
-	fadd_AVX512
-	fadd_AVX512
-
-	sub x0, x0, 1
-	cmp x0, 0
-
-	bne .AVX512Head
-	ret
-
